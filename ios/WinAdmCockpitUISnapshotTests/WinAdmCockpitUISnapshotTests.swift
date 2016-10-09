d6f260a9b523f4e4141c0dfcecbc8c0200ad55d7
David Asabina <david@supr.nu>
2016-10-09 17:27:45 +0200

    better ui tests

--- ios/WinAdmCockpitUISnapshotTests/WinAdmCockpitUISnapshotTests.swift
+++ ios/WinAdmCockpitUISnapshotTests/WinAdmCockpitUISnapshotTests.swift
@@ -22,7 +22,6 @@ class WinAdmCockpitUISnapshotTests: XCTestCase {
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         let app = XCUIApplication()
         app.launch()
-        print(app.debugDescription)
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -34,14 +33,19 @@ class WinAdmCockpitUISnapshotTests: XCTestCase {
 
     func testLoginAndOverviewFlow() {
       let app = XCUIApplication()
-      app.navigationBars["Resultaat"].children(matching: .button).element.tap()
       snapshot(name: "Blank")
+
+      let accountButton = app.navigationBars["Resultaat"].buttons.element(boundBy: 1)
+      print(accountButton.debugDescription)
+
+      accountButton.tap()
 
       let emailField = app.textFields.element
       emailField.typeText("annemiek@abctotaal.nl")
-      //XCTAssert(false, app.buttons["Next"].debugDescription)
+
       app.buttons["Next"].tap()
       app.typeText("geheim")
+
       snapshot(name: "Login")
 
       app.buttons["Go"].tap()
